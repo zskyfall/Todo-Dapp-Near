6cf921f242cb95797414006dd4a1d9c7f7d0ad7f
Nguyễn Hữu Thắng <zestskyfall@gmail.com>
2021-08-27 21:48:04 +0700

    add test

--- contract/assembly/__tests__/main.spec.ts
+++ contract/assembly/__tests__/main.spec.ts
@@ -1,9 +1,18 @@
-import { setGreeting } from '..'
-import { storage, Context } from 'near-sdk-as'
+import { getTasksByAccountId, getAllTasks, addNewTask, toggleTaskDone } from '..'
+import { Task, tasks } from '../model'
+import { storage, Context, logging } from 'near-sdk-as'
 
-describe('Greeting ', () => {
+describe('addNewTask ', () => {
   it('should be set and read', () => {
-    setGreeting('hello world')
-    storage.get<string>(Context.sender)
+       // Store new task into blockchain
+      let tskId = 1;
+      let accountId = Context.sender;
+      let author = 'nguyenhuuthang.dev';
+      let content = 'content.test';
+      let now = Date.now().toString();
+      let tsk = new Task(tskId, author, accountId, content, now, false);
+      let index = tasks.push(tsk);
+      logging.log(tsk);
   })
 })
+
